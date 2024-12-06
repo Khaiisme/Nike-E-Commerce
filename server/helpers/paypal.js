4bd130d34805f9ae6a3d10b12f662332ce1f67a8
Van Khai <kn579309@gmail.com>
2024-12-06 14:44:56 +0100

    Remove <paypal> from version control

--- server/helpers/paypal.js
+++
@@ -1,9 +0,0 @@
-const paypal = require("paypal-rest-sdk");
-
-paypal.configure({
-  mode: 'sandbox', // or 'live'
-  client_id: 'sb-rf4mh34499232@personal.example.com',
-  client_secret: 'VP1sYyO^',
-});
-
-module.exports = paypal;
