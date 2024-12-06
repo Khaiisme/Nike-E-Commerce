d46c9b476c55bcbe1fd5b571c7256d9f1c0a1009
Van Khai <kn579309@gmail.com>
2024-12-06 14:35:39 +0100

    Your descriptive commit messag

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -3,8 +3,8 @@ const multer = require("multer");
 require("dotenv").config();
 cloudinary.config({
   cloud_name:process.env.CLOUDINARY_NAME ,
-  api_key:process.env.CLOUNDINARY_KEY ,
-  api_secret:process.env.CLOUDINARY_SECRET ,
+  api_key:process.env.CLOUDINARY_KEY ,
+  api_secret:process.env.CLOUDINARY_SECRET ,  
 });
 
 const storage = new multer.memoryStorage();
