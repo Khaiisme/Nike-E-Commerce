b1f9c71f109c3071d76b8055b23fc840de50beaa
Van Khai <kn579309@gmail.com>
2024-12-15 00:57:22 +0100

    .

--- server/server.js
+++ server/server.js
@@ -24,9 +24,14 @@ const app = express();
 const PORT = process.env.PORT;
 app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    return res.status(200).json({});
+    res.header("Access-Control-Allow-Credentials", "true");
+    return res.status(204).end(); // End preflight request
   }
   next();
 });
