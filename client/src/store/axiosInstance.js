098894b4960ab1674b3edce69f4c537905d9af4d
Van Khai <kn579309@gmail.com>
2024-12-15 00:10:15 +0100

    .

--- client/src/store/axiosInstance.js
+++ client/src/store/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:5000";
-console.log(REACT_APP_API_URL);
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
